1839f3dd46290236e55afeb9921e38d6b7cfa88f
RainGood <raintube29doesn.tknow@gmail.com>
2023-12-03 22:38:43 +0200

    cleanup

--- src/main/java/me/rain/revivebox/ReviveBox.java
+++ src/main/java/me/rain/revivebox/ReviveBox.java
@@ -6,13 +6,21 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
+import org.bukkit.block.Sign;
+import org.bukkit.block.sign.Side;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public final class ReviveBox extends JavaPlugin implements Listener {
 
@@ -21,66 +29,48 @@ public final class ReviveBox extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
     @EventHandler
-    public void OnJoin(PlayerJoinEvent e){
-        Player player = e.getPlayer();
+    public void OnPlayerDead(PlayerDeathEvent e){
+        Player player = e.getEntity();
         World world = player.getWorld();
+
         int howmanyitems = 0;
-        for (int i = 0; i <= 36; i++){
+        for (int i = 0; i <= 41; i++){
             if (player.getInventory().getItem(i) != null) {
                 howmanyitems++;
             }
         }
+        Block block_left = player.getLocation().getBlock();
+        Block block_right = world.getBlockAt(block_left.getX()-1, block_left.getY() ,block_left.getZ());
+        Block sign_block = world.getBlockAt(block_left.getX(),block_left.getY(),block_left.getZ()+1);
+        sign_block.setType(Material.SPRUCE_SIGN);
+        Sign sign = (Sign) sign_block.getState();
+        sign.getSide(Side.FRONT).setLine(0, "There is a dead body of " + player.getName());
+        sign.update();
 
-        if (e.getPlayer().getInventory().getContents().length <= 27){
+        block_left.setBlockData(Material.CHEST.createBlockData("[facing=south, type=left]"));
+        block_right.setBlockData(Material.CHEST.createBlockData("[facing=south, type=right]"));
 
-            Block block = player.getLocation().getBlock();
-            block.setType(Material.CHEST);
+        Chest chest = (Chest) block_left.getState();
+        Chest chest2 = (Chest) block_right.getState();
+        Inventory inventory_chest = chest.getBlockInventory();
+        Inventory inventory_chest2 = chest2.getBlockInventory();
+        int inv_num = 0;
+        for (int i = 0; i <= howmanyitems; i++){
+            if (player.getInventory().getItem(i) != null){
+                try {
+                    if (i<=27){
+                        inventory_chest2.setItem(inv_num, player.getInventory().getItem(i));
+                        inv_num++;
 
-            Chest chest = (Chest) block.getState();
-            Inventory inventory_chest = chest.getBlockInventory();
-            int inv_num = 0;
-            for (int i = 0; i <= howmanyitems; i++){
-                if (player.getInventory().getItem(i) == null)
-                {
-                    player.sendMessage("Empty");
-                }else{
-                    inventory_chest.setItem(inv_num, player.getInventory().getItem(i));
-                    inv_num++;
-                }
-
-            }
-        }else{
-            Block block_left = player.getLocation().getBlock();
-            Block block_right = world.getBlockAt(block_left.getX()-1, block_left.getY() ,block_left.getZ());
-
-            block_left.setBlockData(Material.CHEST.createBlockData("[facing=south, type=left]"));
-            block_right.setBlockData(Material.CHEST.createBlockData("[facing=south, type=right]"));
-
-            Chest chest = (Chest) block_left.getState();
-            Chest chest2 = (Chest) block_right.getState();
-            Inventory inventory_chest = chest.getBlockInventory();
-            Inventory inventory_chest2 = chest2.getBlockInventory();
-            int inv_num = 0;
-            for (int i = 0; i <= howmanyitems; i++){
-                if (player.getInventory().getItem(i) == null)
-                {
-                    player.sendMessage("Empty");
-                }else{
-                    try {
-                        if (i<=27){
-                            inventory_chest2.setItem(inv_num, player.getInventory().getItem(i));
-                            inv_num++;
-
-                        }else {
-                            inventory_chest.setItem(inv_num, player.getInventory().getItem(i));
-                            inv_num++;
-                        }
-                    } catch (ArrayIndexOutOfBoundsException ex) {
-                        inv_num = 0;
+                    }else {
+                        inventory_chest.setItem(inv_num, player.getInventory().getItem(i));
+                        inv_num++;
                     }
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                        inv_num = 0;
                 }
-
             }
+
         }
     }
 }
